7d713d05a7ed934577c625da926c09cf457206cd
PlasmaShine <norad79@yahoo.com>
2015-02-22 12:20:43 +0900

    First commit

---
+++ CleanCart/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CleanCart
+//
+//  Created by Nemes Norbert on 2/22/15.
+//  Copyright (c) 2015 Norbert Nemes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CleanCart/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CleanCart
+//
+//  Created by Nemes Norbert on 2/22/15.
+//  Copyright (c) 2015 Norbert Nemes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
