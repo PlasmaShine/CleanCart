5ef22ad8933195d53094f373a165c51378bfdf6f
Norbert Nemes <norad79@yahoo.com>
2015-03-02 18:01:10 +0900

    Created view controller

---
+++ CleanCart/ListItemsViewController.h
@@ -0,0 +1,13 @@
+//
+//  ListItemsViewController.h
+//  CleanCart
+//
+//  Created by Nemes Norbert on 3/2/15.
+//  Copyright (c) 2015 Norbert Nemes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ListItemsViewController : UICollectionViewController
+
+@end
---
+++ CleanCartTests/ListItemsViewControllerTests.m
@@ -0,0 +1,38 @@
+//
+//  ListItemsViewControllerTests.m
+//  CleanCart
+//
+//  Created by Nemes Norbert on 3/2/15.
+//  Copyright (c) 2015 Norbert Nemes. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import "ListitemsViewController.h"
+
+@interface ListItemsViewControllerTests : XCTestCase
+
+@end
+
+@implementation ListItemsViewControllerTests
+
+- (void)setUp {
+    [super setUp];
+}
+
+- (void)tearDown {
+    [super tearDown];
+}
+
+#pragma mark - Tests
+
+- (void)testViewControllerConformsToProperProtocols {
+    ListItemsViewController *sut = [[ListItemsViewController alloc] init];
+    
+}
+
+- (void)testLoadingTheViewSendsViewLoadMessageToPresenter {
+    
+}
+
+
+@end
